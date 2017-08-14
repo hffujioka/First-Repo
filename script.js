80af70e1c9df4de019512bcc6dc99997fe05c4df
hffujioka <30875167+hffujioka@users.noreply.github.com>
2017-08-14 09:08:27 -1000

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function sayHello() {
   //I create a function called sayHello//
-  var userName = prompt("What is your name?");
-  //I create a variable called userName and ask the user something//
-  window.alert("Hello " + userName);
+  var firstName = prompt("What is your first name?");
+  console.log(firstName);
+  //I create a variable called firstName and ask the user something//
+  var nickName = prompt("What is your nickname?");
+  //I create a variable called nickName and ask the user something//
+  var lastName = prompt("What is your last name?");
+  //I create a variable called lastName and ask the user something//
+  window.alert("Hello " + firstName + " " + nickName + " " + lastName);
   //I create a popup window telling the userName hello//
 }
