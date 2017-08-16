fb567ad2ef33116215f939538098eee0d4f6ebf2
hffujioka <30875167+hffujioka@users.noreply.github.com>
2017-08-16 08:45:02 -1000

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var favFoods = ["bacon", "Lettuce", "Tomato"]; //Made an array called favFoods with 3 values
+
+var myChoice = 1; //Made variable myChoice equal 1
+
+window.alert(favFoods[myChoice]); //Array favFoods calls the item in position 1 in a pop up
+
+window.alert(favFoods.length); //A pop up tells the number of items in favFoods
+
+var students = ["Michael", "Raene", "Chuck", "Ryan"]; //Made an array students
+
+window.alert(students[2] + " " + favFoods[0]); //Item 2 from students and 0 from favFoods are called to a pop up
